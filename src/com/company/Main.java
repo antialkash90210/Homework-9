5423b9a09c29a912f8706a385f913fa3e95cd3ba
ConcoleFirst <alek.kalashnikov32@yandex.ru>
2021-06-02 21:37:43 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        Random rnd = new Random();
+        int numberOfAttempts = 0;
+        int compNumber;
+        int userNumber = 0;
+        final int EXIT_CODE = -1;
+        boolean inputResult;
+
+        compNumber = rnd.nextInt(10) + 1;
+
+        do {
+            System.out.println("Введите любое число от 1 до 10: ");
+            userNumber = input.nextInt();
+
+            if (userNumber > compNumber) {
+                System.out.println("Введите число поменьше: ");
+            } else if (userNumber < compNumber) {
+                System.out.println("Введите число побольше: ");
+            }
+
+            if (userNumber != EXIT_CODE) {
+                numberOfAttempts++;
+            }
+
+
+        } while (compNumber != userNumber);
+
+        System.out.println("Поздравляю,вы угадали число!! ");
+        System.out.println("Количество попыток: " + numberOfAttempts);
+
+        if (numberOfAttempts <= 3) {
+            System.out.println("гений");
+        } else if (numberOfAttempts > 3 && numberOfAttempts <= 6) {
+            System.out.println("среднячок");
+        } else {
+            System.out.println("лошара");
+        }
+
+    }
+}
