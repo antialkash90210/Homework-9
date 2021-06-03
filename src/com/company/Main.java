ccb5fd20d1a2c126facb2924eb2f2b6003b570e8
ConcoleFirst <alek.kalashnikov32@yandex.ru>
2021-06-03 14:47:17 +0300

    ++

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,25 +7,53 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Scanner input = new Scanner(System.in);
         Random rnd = new Random();
         int numberOfAttempts = 0;
-        int compNumber;
+        int compNumber = 0;
         int userNumber = 0;
         final int EXIT_CODE = -1;
         boolean inputResult;
 
-        compNumber = rnd.nextInt(10) + 1;
+        int difficulty;
+        boolean inputDifficulty;
+
+        try {
+            inputDifficulty = true;
+            System.out.println("Введите сложность игры от 1 до 3: ");
+            difficulty = input.nextInt();
+
+            switch (difficulty) {
+                case 1:
+                    System.out.println("легко ");
+                    System.out.println("Введите любое число от 1 до 10: ");
+                    compNumber = rnd.nextInt(10) + 1;
+                    break;
+                case 2:
+                    System.out.println("средне ");
+                    System.out.println("Введите любое число от 1 до 50: ");
+                    compNumber = rnd.nextInt(50) + 1;
+                    break;
+                case 3:
+                    System.out.println("сложно ");
+                    System.out.println("Введите любое число от 1 до 100: ");
+                    compNumber = rnd.nextInt(100) + 1;
+                    break;
+            }
+        }
+        catch (Exception e) {
+            inputDifficulty = false;
+            System.out.println("Ошибка ввода,перезапустите программу " + e.getMessage());
+        }
 
         do {
             inputResult = true;
-            Scanner input = new Scanner(System.in);
+//           Scanner input = new Scanner(System.in);
 
             try {
-                System.out.println("Введите любое число от 1 до 10: ");
                 userNumber = input.nextInt();
 
-                if (userNumber < 1 || userNumber > 10) {
+                if (userNumber < 1 || userNumber > 100) {
                     throw new Exception();
                 }
 
@@ -39,7 +67,6 @@ public class Main {
                     numberOfAttempts++;
                 }
             }
-
             catch (Exception e) {
                 inputResult = false;
             }
@@ -56,6 +83,5 @@ public class Main {
         } else {
             System.out.println("лошара");
         }
-
     }
 }
